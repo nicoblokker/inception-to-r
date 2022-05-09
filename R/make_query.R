0b4276e7ab72d5aac634c51b4c3e2f5792838f10
Nico Blokker <blokker@uni-bremen.de>
2022-05-09 07:50:15 +0200

    refactoring make_query

--- R/make_query.R
+++ R/make_query.R
@@ -10,8 +10,8 @@ make_query <- function(keywords){
           query <- vector()
           for (i in keywords) {
                     query[i] <- paste0("contains(name(),", "'", i, "')") 
-                    query2 <- paste(query, collapse = " or ")
-                    query3 <- paste0("//*[", query2,  "]")
           }
+          query2 <- paste(query, collapse = " or ")
+          query3 <- paste0("//*[", query2,  "]")
           return(query3)
 }
