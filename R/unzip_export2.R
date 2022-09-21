eaca5c0e948f23ab0c315b9ee6802d9b0520575e
Nico Blokker <blokker@uni-bremen.de>
2022-09-21 07:47:48 +0200

    added updated function for export

---
+++ R/unzip_export2.R
@@ -0,0 +1,28 @@
+#' @title unzip_export2
+#' @description unzips files exported from inception (creates local files; use with caution). Same usage as `unzip_export` but adapted to work with more recent exports.
+#' @param path_to_export set path to where the xmi-files are stored
+#' @param overwrite should existing unzipped files be unzipped again (defaults to FALSE)
+#' @param recursive should files from sub-folders also be unzipped (defaults to FALSE)
+#' @return unzips files locally and provides list with paths to extracted files
+#' @examples
+#' \dontrun{
+#' unzip_export2(path_to_export = ".")
+#' }
+#' @export
+
+unzip_export2 <- function(path_to_export = ".", overwrite = FALSE, recursive = FALSE){
+          annotated_docs <- list.files(path_to_export, recursive = recursive)
+          annotated_docs <- annotated_docs[grepl("webanno|inception\\d+", annotated_docs)]
+          doc_names <- list()
+          for(i in seq_along(annotated_docs)){
+                    path <- paste0(path_to_export, "\\", annotated_docs[i])
+                    out <- gsub("webanno.*?$|inception.*?$", "extracted", path)
+                    doc_names[[i]] <- utils::unzip(path, exdir = out, overwrite = overwrite)[1]
+          }
+          if(length(doc_names) > 0){
+                    message("extraction complete...")
+          } else {
+                    message("no valid documents found")
+          }
+          doc_names_complete <- unlist(doc_names)
+}
