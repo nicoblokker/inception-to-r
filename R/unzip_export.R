9886bea4e2e16592c1c433529518c7d641bb48d3
Sebastian Haunss <sebastian.haunss@uni-bremen.de>
2023-05-12 07:30:24 +0200

    updated unzip function that takes care of altered export folder structure and now saves unzipped files in folder "extracted"

--- R/unzip_export.R
+++ R/unzip_export.R
@@ -1,23 +1,25 @@
 #' @title unzip_export
 #' @description unzips files exported from inception (creates local files; use with caution)
-#' @param path_to_export set path to where the xmi-files are stored
+#' @param path path of the folder that contains the Inception export
+#' @param folder name of the folder withing the Inception export, in which the xmi files with the annotations are stored
 #' @param overwrite should existing unzipped files be unzipped again (defaults to FALSE)
 #' @param recursive should files from sub-folders also be unzipped (defaults to FALSE)
-#' @return unzips files locally and provides list with paths to extracted files
+#' @return unzips files locally in the subfolder "extracted" and provides list with paths to extracted files
 #' @examples
 #' \dontrun{
-#' unzip_export(path_to_export = ".")
+#' unzip_export(path = "NameOfExportFolder", folder = "annotation")
 #' }
 #' @export
 
-unzip_export <- function(path_to_export = ".", overwrite = FALSE, recursive = FALSE){
-          annotated_docs <- list.files(path_to_export, recursive = recursive)
-          annotated_docs <- annotated_docs[grepl("webanno", annotated_docs)]
+unzip_export <- function(path, folder, overwrite = FALSE, recursive = FALSE){
+          path_to_import <- paste0(path, "/", folder)
+          path_to_export <- paste0(path, "/extracted")
+          annotated_docs <- list.files(path_to_import, recursive = recursive) %>% grep(pattern = "INITIAL_CAS.zip", invert = T, value = T)
           doc_names <- list()
           for(i in seq_along(annotated_docs)){
-                    path <- paste0(path_to_export, "\\", annotated_docs[i])
-                    out <- gsub("webanno.*?$", "extracted", path)
-                    doc_names[[i]] <- utils::unzip(path, exdir = out, overwrite = overwrite)[1]
+                    out = gsub("(\\w+).xmi/(\\w+).zip", perl = TRUE, replacement = "\\1/\\2", annotated_docs[i])
+                    doc_names[[i]] <- utils::unzip(paste0(path_to_import, "/", annotated_docs[i]), list = T)
+                    utils::unzip(paste0(path_to_import, "/", annotated_docs[i]), exdir = paste0(path_to_export, "/", out), overwrite = overwrite)
           }
           if(length(doc_names) > 0){
                     message("extraction complete...")
