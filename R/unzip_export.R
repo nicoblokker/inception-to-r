e1caae5031a26dd6cb8bf80bc12f3a13ac345c25
Nico Blokker <blokker@uni-bremen.de>
2023-05-15 11:59:37 +0200

    updated export & xmi2list function

--- R/unzip_export.R
+++ R/unzip_export.R
@@ -1,22 +1,22 @@
 #' @title unzip_export
 #' @description unzips files exported from inception (creates local files; use with caution)
-#' @param path_to_export set path to where the xmi-files are stored
+#' @param folder set path to folder in which the xmi-files are stored
 #' @param overwrite should existing unzipped files be unzipped again (defaults to FALSE)
 #' @param recursive should files from sub-folders also be unzipped (defaults to FALSE)
 #' @return unzips files locally and provides list with paths to extracted files
 #' @examples
 #' \dontrun{
-#' unzip_export(path_to_export = ".")
+#' unzip_export(folder = "export")
 #' }
 #' @export
 
-unzip_export <- function(path_to_export = ".", overwrite = FALSE, recursive = FALSE){
-          annotated_docs <- list.files(path_to_export, recursive = recursive)
-          annotated_docs <- annotated_docs[grepl("webanno|inception\\d+", annotated_docs)]
+unzip_export <- function(folder, overwrite = FALSE, recursive = FALSE){
+          annotated_docs <- list.files(folder, recursive = recursive)
+          annotated_docs <- annotated_docs[!grepl("INITIAL_CAS", annotated_docs) & grepl("annotation\\W|curation\\W", annotated_docs)]
           doc_names <- list()
           for(i in seq_along(annotated_docs)){
-                    path <- paste0(path_to_export, "\\", annotated_docs[i])
-                    out <- gsub("webanno.*?$|inception.*?$", "extracted", path)
+                    path <- file.path(folder,  annotated_docs[i])
+                    out <- file.path(gsub("(\\w+)\\.?\\w+$", "", annotated_docs[i]), "extracted")
                     doc_names[[i]] <- utils::unzip(path, exdir = out, overwrite = overwrite) |> (\(.) {.[!grepl("TypeSystem", .)]})()
           }
           if(length(doc_names) > 0){
